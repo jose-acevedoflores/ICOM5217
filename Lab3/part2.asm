14f14b0426404099aa2495eaf0423f116f0c5ee2
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-03-03 18:34:37 -0400

    added part2

---
+++ Lab3/part2.asm
@@ -0,0 +1,49 @@
+#include "msp430.h"                     ; #define controlled include file
+#define DELAY_ARGS      R5
+
+
+        NAME    main                    ; module name
+
+        PUBLIC  main                    ; make the main label vissible
+                                        ; outside this module
+        ORG     0FFFEh
+        DC16    init                    ; set reset vector to 'init' label
+        
+        ORG     0x0FFEC                 
+        DC16    timer                   ; Set timer ISR
+
+        RSEG    CSTACK                  ; pre-declaration of segment
+        RSEG    CODE                    ; place program in 'CODE' segment
+
+init:   MOV     #SFE(CSTACK), SP        ; set up stack
+
+main:   NOP                             ; main program
+        MOV.W   #WDTPW+WDTHOLD,&WDTCTL  ; Stop watchdog timer
+        
+        bis.b   #BIT5,P8DIR             ; Set P8.5 as output for buzzer
+        bis.b   #BIT5,P8OUT             ; Send high signal to buzzer
+        bis.b   #BIT0,P1DIR             ; Set P1.0 as output for LED
+        bis.b   #BIT0,P1OUT             ; Turn LED on
+        
+        bis.w   #SELA_2+SELS_2,&UCSCTL4       ; Set SMCLK source to REFOCLOCK
+        
+        bis.w   #TASSEL_1 + MC_1 + ID_3,&TA0CTL ; Set timer, SMCLK source, Up count operation and divide input signal by 4
+        bis.w   #CCIE, &TA0CCTL0                ; Enable interrupt on compare
+        bis.w   #0001h, &TA0CCR0                ; Count up to FFFF
+        bis.w   #TAIDEX_2, &TA0EX0              ; Divide input signal by 4
+        eint
+        
+loop:   nop                             ; Infinite loop for waiting interrupt
+        jmp loop
+        
+///////////////////////////////////////////////////////////////////////////////
+// Timer Interrupt Service Routine
+///////////////////////////////////////////////////////////////////////////////
+
+timer:  xor.b   #BIT5,P8OUT             ; Toggle status of buzzer signal
+        xor.b   #BIT0,P1OUT             ; Toggle LED
+        bic.w   #BIT0,TA0CTL            ; Clear interrupt flag
+        reti
+                                                
+        END
+        
