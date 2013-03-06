00e9a85bdbe2d8f6523f5c124dcd53f0a37b8112
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-03-06 02:57:35 -0400

    Digital Tach Direction V2

--- Lab-3/HomeWork/digitalTach.asm
+++ Lab-3/HomeWork/digitalTach.asm
@@ -2,8 +2,7 @@
 #include "../header_files/lcd.h"
 #include  "timer.h"
 #define STATUS    R7
-
-
+    
         NAME    main                    ; module name
 
         PUBLIC  main                    ; make the main label vissible
@@ -16,7 +15,10 @@
         
         ORG     0x0FFDE
         DW      BPIR                    ; Init interrupt vector for button (P1.6 Track A and P1.7 Track B)
-
+        
+        ORG     0x02c00
+DIR     DB      0                       ; Place in ram to store the current direction (Forward = 1 or backward= 0 )        
+        
         RSEG    CSTACK                  ; pre-declaration of segment
         RSEG    CODE                    ; place program in 'CODE' segment
 
@@ -28,6 +30,8 @@ main:   NOP                             ; main program
         call    #InitPort               ; Initialize ports
         call    #INIT_LCD               ; Initialize LCD
         clr     STATUS
+        clr     DIR
+        clr     R14
         
 loop    eint                             ; Infinite loop for waiting interrupt
         jmp loop
@@ -41,20 +45,27 @@ LookUPT mov.w   #JMPTABL, R5
         jz      BACKWARD                ; CW
         jmp     loop
 
-FORWARD mov.b   #0xC0,COM_ARGS		; New Line
+FORWARD cmp.b   #1, R14
+        jz      loop
+        mov.b   #1, R14
+        mov.b   #0xC0,COM_ARGS		; New Line
         call    #WRITECOM		; 
         mov.w   #ForwardLabel, STR
         call    #WRITESTR
         jmp     loop
-BACKWARD mov.b   #0xC0,COM_ARGS		; New Line
-         call    #WRITECOM		; 
+        
+BACKWARD cmp.b  #0,R14
+         jz     loop 
+         mov.b  #0,R14
+         mov.b  #0xC0,COM_ARGS		; New Line
+         call   #WRITECOM		; 
          mov.w  #BackwardLabel,STR
          call   #WRITESTR
          jmp    loop
 ///////////////////////////////////////////////////////////////////////////////
 //Light Interrupt Service Routine
 ///////////////////////////////////////////////////////////////////////////////        
-BPIR    push.w    R5                      ; Save R5 to use it as a temp variable
+BPIR    push.w  R5                      ; Save R5 to use it as a temp variable
         bit.b   #BIT7,P1IFG 	        ; check if 1.7  generated the flag
         jnz     TOG7
         bit.b   #BIT6,P1IFG 	        ; check if 1.6 generated the flag
@@ -62,41 +73,25 @@ BPIR    push.w    R5                      ; Save R5 to use it as a temp variable
         jmp     GOBACK          	; Something else generated the interrupt
         
 TOG7    xor.b   #BIT7,P1IES  
-        jmp     TrakB
+        jmp     Rotate
 TOG6    xor.b   #BIT6, P1IES
         
-TrakB   nop
+Rotate  push.b  P1IN
         mov.w   STATUS,R5               ; Save status in R5
         rla.b   R5
-        rla.b   R5                      ; Rotate Bnew to Bold position
-        bic.b   #11111011b,R5           ; set all bits to zero except Bold
-        bic.b   #00000100b, STATUS      ; Clear Bold in STATUS
-        add.b   R5,STATUS               ; Move Bnew to Bold
-        mov.b   P1IN,R5
-        bic.b   #01111111b,R5           ; Clear all PINs except PIN1.7
-        rla.b   R5
-        rlc.b   R5                      ; x0000000 -> 0000000x move PIN1.7 to Bnew position
-        bic.b   #00000001b,STATUS       ; Clear Bnew 
-        add.b   R5,STATUS               ; Save P1.7 IN in Bnew
-        bic.b   #BIT7, P1IFG            ; Clear Interrupt flag
+        rla.b   R5                      ; Rotate Bnew to Bold position and Anew to Aold
+        bic.b   #11110011b,R5           ; set all bits to zero except Bold and Aold
+        bic.b   #00001100b, STATUS      ; Clear Bold and Aold in STATUS
+        add.b   R5,STATUS               ; Move Bnew to Bold and Anew to Aold in Status 
+        pop     R5
+        bic.b   #00111111b,R5           ; Clear all PINs except PIN1.7 and P1.6
+        rla.b   R5                      ; xx000000 -> x0000000 C = x
+        rlc.b   R5                      ; x0000000 -> 0000000x C = x
+        rlc.b   R5                      ; xx000000 -> 000000xx move PIN1.7 to Bnew position
+        bic.b   #00000011b,STATUS       ; Clear Bnew and Anew in status
+        add.b   R5,STATUS               ; Save P1.7 IN in Anew and P1.6 in Bnew 
+        bic.b   #BIT7+BIT6, P1IFG            ; Clear Interrupt flag
         
-TrakA   nop
-        mov.w   STATUS,R5               ; Save status in R5
-        rla.b   R5
-        rla.b   R5                      ; Rotate Anew to Aold position
-        bic.b   #11110111b,R5           ; set all bits to zero except Aold
-        bic.b   #00001000b, STATUS      ; Clear Aold in STATUS 
-        add.b   R5,STATUS               ; Move Anew to Aold
-        mov.b   P1IN,R5
-        bic.b   #10111111b,R5           ; Clear all PINs except PIN1.6
-        rla.b   R5                      ; 0x000000 -> x0000000 move PIN1.6 to Anew position
-        rla.b   R5
-        rlc.b   R5                      ; x0000000 -> 0000000x
-        rla.b   R5                      ; 0000000x -> 000000x0
-        bic.b   #00000010b,STATUS       ; Clear Anew 
-        add.b   R5,STATUS               ; Save PIN1.6 in Anew 
-        bic.b   #BIT6, P1IFG            ; Clear Interrupt flag
-
 GOBACK  pop     R5                      ; Recuperate R5 contents
         mov.w   #LookUPT, 2(SP)   	; Modify the new return address
         bic.b   #GIE, 0(SP)             ; Disale interrupts 
