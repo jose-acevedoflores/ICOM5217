be52eb37148414c0d3ba1760cb320d02fa94edcf
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-03-12 22:51:54 -0400

    GUI Work started

--- Phoenix3D/src/main/view/MainFrame.java
+++ Phoenix3D/src/main/view/MainFrame.java
@@ -1,5 +1,15 @@
 package main.view;
 
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.SequentialGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  * This class will represent a Phoenix3D application frame.
  * An object from this class will contain:
@@ -12,6 +22,102 @@ package main.view;
  * @author joseacevedo
  *
  */
-public class MainFrame {
+public class MainFrame extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JButton importB;
+	private JButton printB;
+	
+	private JComboBox layerThickness;
 
+	private JLabel logo;
+	private JLabel numOfLayers;
+	private JLabel eta;
+	
+	private GroupLayout layout;
+	private JPanel panel; 
+	/**
+	 * Initialize all the components that will be displayed in the frame 
+	 */
+	public MainFrame()
+	{
+		super("Phoenix DLP 3D Printer");
+		this.importB = new JButton("Import");
+		this.printB = new JButton("Print");
+		
+		String[] options = { "1.5mm","1mm", "0.5mm"};
+		this.layerThickness = new JComboBox(options);
+		
+		this.logo = new JLabel(new ImageIcon("resources/logoS.png"));
+		this.numOfLayers = new JLabel("Number of layers: ");
+		this.eta = new JLabel("Estimated Printing Time: ");
+		
+	
+		this.initLayout();
+		
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600, 400);
+		this.add(panel);
+		
+	}
+	
+	/**
+	 * This method sets the layout of the elements that will be shown to the user
+	 */
+	private void initLayout()
+	{
+		this.panel = new JPanel();
+		this.layout = new GroupLayout(this.panel);
+		
+		SequentialGroup group;
+		
+		//Set Horizontal Alignment
+		group = this.layout.createSequentialGroup();
+		
+		//Add components
+		group.addGap(20);
+		group.addComponent(this.importB);
+		group.addGap(260);
+		group.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(this.logo)
+				.addComponent(this.numOfLayers)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+						.addComponent(this.eta)
+						.addComponent(this.printB)
+						)
+
+				);
+		
+		
+		this.layout.setHorizontalGroup(group);
+		
+		//Set Vertical Alignment 
+		group = this.layout.createSequentialGroup();
+		
+		//Add components 
+		group.addGap(20);
+		group.addGroup(layout.createParallelGroup()
+		.addComponent(this.importB)
+		.addComponent(this.logo)
+				);
+		group.addGap(120);
+	
+		group.addComponent(this.numOfLayers);
+		group.addGap(20);
+		group.addComponent(this.eta);
+		group.addGap(40);
+		group.addComponent(this.printB);
+		
+		this.layout.setVerticalGroup(group);
+		
+		
+		//Add the layout to the panel
+		panel.setLayout(layout);
+	}
+	
+	
 }
