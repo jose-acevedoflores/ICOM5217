d722e916f0dead94cbff0ee68d23155c8e4c67be
Jose Acevedo_Virtual Box <acevedoflores.jose@gmail.com>
2013-03-26 18:26:14 -0400

    Progress bar working

--- Phoenix3D/src/main/freesteel/FreeSteelSlice.java
+++ Phoenix3D/src/main/freesteel/FreeSteelSlice.java
@@ -1,8 +1,14 @@
 package main.freesteel;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
+
+import javax.swing.ProgressMonitor;
+import javax.swing.SwingWorker;
 
 import main.view.MainFrame;
 
@@ -26,6 +32,9 @@ public class FreeSteelSlice {
 	private String currentPath;
 	private MainFrame viewReferences; 
 
+	private ProgressMonitor progressMonitor;
+	private static Task task;
+
 	/**
 	 * Creates a new FreeSteel Runtime component to access the Python script of FreeSteel
 	 */
@@ -41,52 +50,116 @@ public class FreeSteelSlice {
 	 */
 	public void slice()
 	{
-		
+
 		if(System.getProperty("os.name").equals("Linux"))
 		{
 			if(!STL_FILE_NAME.equals("!") )
 			{
 				//System.out.println(STL_FILE_NAME+" -- "+LAYER_THICKNESS);
-				
-				try {
-					currentPath = System.getProperty("user.dir");
-					
-					String scriptLocation = currentPath+"/"+sliceScriptPath;
-					String options = "-z -15,150,0.5";
-					String outputLocation = currentPath+"/"+freeSteelOutput+"test.bmp";
-					String cmd[] = {"python", scriptLocation, options,
-							STL_FILE_NAME, "-o", outputLocation};
-					
-					for(String str : cmd)
-						System.out.println(str);
-					
-					Process p = Runtime.getRuntime().exec(cmd);
-					BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					
-					
-					while(br.ready())
-						System.out.println(br.readLine());
-					
-					p.waitFor();
-					System.out.println("wut");
 
-				} catch (IOException e) {
-				
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					
-					e.printStackTrace();
-				} 
-				
-				//The substring here gets the original content of the JLabel (Number of layers:) and adds the new computed number of layers
-				viewReferences.numOfLayers.setText(viewReferences.numOfLayers.getText().substring(0, 17)+ " 200");
+				progressMonitor = new ProgressMonitor(viewReferences, "Running a Long Task",
+						"", 0, 100);
+				task = new Task();
+				task.addPropertyChangeListener(new ProgressListener());
+				task.execute();
+
 			}
 			else 
 				System.out.println("File or layer thickness not initialized");
 		}
 		else
 			System.out.println(System.getProperty("os.name")+" is not supported by freeSteel");
-		
+
+
+	}
+
+	private class Task extends SwingWorker<Void, Void>
+	{
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			setProgress(0);
+			try {
+				currentPath = System.getProperty("user.dir");
+
+				String scriptLocation = currentPath+"/"+sliceScriptPath;
+				String options = "-z -15,150,"+LAYER_THICKNESS;
+				String outputLocation = currentPath+"/"+freeSteelOutput+"test.bmp";
+				String cmd[] = {"python", scriptLocation, options,
+						STL_FILE_NAME, "-o", outputLocation};
+
+				for(String str : cmd)
+					System.out.println(str);
+
+
+				Process p = Runtime.getRuntime().exec(cmd);
+				//	BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+
+				//				while(br.ready())
+				//					System.out.println(br.readLine());
+				int progress =0;
+				Random random = new Random();
+				while (progress < 100 && !isCancelled()) {
+					//Sleep for up to one second.
+					Thread.sleep(random.nextInt(1000));
+					//Make random progress.
+					progress += random.nextInt(10);
+					setProgress(Math.min(progress, 100));
+				}
+
+				System.out.println(p.waitFor());
+
+				System.out.println("wut");
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			} 
+			return null;
+
+		}
+
+		@Override
+		public void done()
+		{
+			System.out.println("Woot WOot");
+			progressMonitor.setProgress(0);
+			//The substring here gets the original content of the JLabel (Number of layers:) and adds the new computed number of layers
+			viewReferences.numOfLayers.setText(viewReferences.numOfLayers.getText().substring(0, 17)+ " 200");
+		}
+
+
+	}
+	private class ProgressListener implements PropertyChangeListener{
+
+		@Override
+		public void propertyChange(PropertyChangeEvent evt) {
+
+			System.out.println("-_- "+evt.getPropertyName());
+			if(evt.getPropertyName().equals("progress"))
+			{			
+				System.out.println("meh "+(Integer) evt.getNewValue());
+
+				progressMonitor.setProgress((Integer) evt.getNewValue());
+				String message =
+						String.format("Completed %d%%.\n", (Integer) evt.getNewValue());
+				progressMonitor.setNote(message);
+				if (progressMonitor.isCanceled() || task.isDone()) 
+				{
+
+					if (progressMonitor.isCanceled()) 
+					{
+						task.cancel(true);
+						System.out.println("Task cancelled");
+					} 
+					else 
+						System.out.println("Task Completed");
+
+
+				}
+			}
+		}
 
 	}
 
