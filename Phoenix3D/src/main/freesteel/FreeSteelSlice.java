5583bfb2eb7b14cd4c30320dbcae95ac8a486f0e
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-03-27 15:04:38 -0400

    Commenting code

--- Phoenix3D/src/main/freesteel/FreeSteelSlice.java
+++ Phoenix3D/src/main/freesteel/FreeSteelSlice.java
@@ -3,6 +3,7 @@ package main.freesteel;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import javax.swing.ProgressMonitor;
 import javax.swing.SwingWorker;
@@ -31,7 +32,7 @@ public class FreeSteelSlice {
 
 	private ProgressMonitor progressMonitor;
 	private static Task task;
-
+	//TODO Spawn only one task (static)
 	/**
 	 * Creates a new FreeSteel Runtime component to access the Python script of FreeSteel
 	 */
@@ -71,7 +72,7 @@ public class FreeSteelSlice {
 	}
 
 	/**
-	 * 
+	 * This class is in charge of executing the python script
 	 * @author jose
 	 *
 	 */
@@ -82,33 +83,51 @@ public class FreeSteelSlice {
 		protected Void doInBackground() throws Exception {
 			setProgress(0);
 			try {
+				//Set the parameters for the python script
 				currentPath = System.getProperty("user.dir");
-
 				String scriptLocation = currentPath+"/"+sliceScriptPath;
 				String options = "-z -15,130,"+LAYER_THICKNESS;
 				String outputLocation = currentPath+"/"+freeSteelOutput+"test.bmp";
 				String cmd[] = {"python", scriptLocation, options,
 						STL_FILE_NAME, "-o", outputLocation};
 
+				//DEBUG: Show all the command parameters 
 				for(String str : cmd)
 					System.out.println(str);
 
-
+				//Execute the script
 				Process p = Runtime.getRuntime().exec(cmd);
+				
+				//Path where the BMPs are stored 
 				File freeSteelBMPs = new File(currentPath+"/"+freeSteelOutput);
+				
+				//Variables to determinate the percentage of completion 
 				String[] bmps;
 				int progress =0;
 				double size = (135/LAYER_THICKNESS);
 				
+				//Filter to pick only the STL files stores at freeSteelBMPs (BMPs storage space) 
+				FilenameFilter t = new FilenameFilter() {
+					
+					@Override
+					public boolean accept(File arg0, String arg1) {
+						return arg1.endsWith(".stl");
+					}
+				};
+				
+				//Loop for calculating completion time
 				while (progress < 100 && !isCancelled()) {
 						
 					bmps = freeSteelBMPs.list();
-					//Sleep for up to one second.
+					
+					System.out.println(bmps.length);
+					//Sleep for one second.
 					Thread.sleep(1000);
 					progress = (int) (bmps.length / size*100);
 					setProgress(progress);
 				}
 
+				//Wait for the execution 
 				System.out.println("Return value "+p.waitFor());
 
 			} catch (IOException e) {
@@ -122,7 +141,6 @@ public class FreeSteelSlice {
 		@Override
 		public void done()
 		{
-			System.out.println("Woot WOot");
 			progressMonitor.setProgress(100);
 			//The substring here gets the original content of the JLabel (Number of layers:) and adds the new computed number of layers
 			viewReferences.numOfLayers.setText(viewReferences.numOfLayers.getText().substring(0, 17)+ " 200");
@@ -132,7 +150,8 @@ public class FreeSteelSlice {
 	}
 	
 	/**
-	 * 
+	 * This class listens to the setProgress(int) of the Task object and displays the changes on the 
+	 * progress dialog
 	 * @author jose
 	 *
 	 */
