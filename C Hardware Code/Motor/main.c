88d621ca1b421c875fd82e25a860d4aefff2caf3
Fernando Lopez <fr.lopez4@gmail.com>
2013-03-27 00:23:13 -0400

    Added Motor.h

--- C Hardware Code/Motor/main.c
+++ C Hardware Code/Motor/main.c
@@ -1,65 +1,10 @@
 #include <msp430.h> 
-
+#include "Motor.h"
 /*
  * main.c
  */
 
-enum SM{
-	FULLSTEP,
-	HALFSTEP,
-	QUARTERSTEP,
-	EIGHTHSTEP,
-	SIXTEENTHSTEP,
-	THIRTYSECONDTHSTEP
-} stepMode;
 
-void wait(int delay){
-	volatile unsigned int i = 0;
-			for( ; i < delay; i++);
-}
-void motorStep(int numberSteps, int direction){
-	if(direction == 0){
-		P3OUT &= ~(0x01);
-	}
-	else{
-		P3OUT |= 0x01;
-	}
-
-    volatile unsigned int i = 0;
-    for(; i < numberSteps; i++){
-    	P3OUT |= 0x02;
-    	wait(500);
-    	P3OUT &= ~(0x02);
-    	wait(500);
-    }
-}
-
-void microSteppingMode(enum SM stepMode){
-	switch(stepMode){
-	case FULLSTEP:
-		P3OUT &= ~(0x01C);
-		break;
-	case HALFSTEP:
-		P3OUT |= 0x010;
-		P3OUT &= ~(0x0C);
-		break;
-	case QUARTERSTEP:
-		P3OUT |= 0x08;
-		P3OUT &= ~(0x14);
-		break;
-	case EIGHTHSTEP:
-		P3OUT |= 0x018;
-		P3OUT &= ~(0x04);
-		break;
-	case SIXTEENTHSTEP:
-		P3OUT |= 0x04;
-		P3OUT &= ~(0x18);
-		break;
-	case THIRTYSECONDTHSTEP:
-		P3OUT |= 0x01C;
-		break;
-	}
-}
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
